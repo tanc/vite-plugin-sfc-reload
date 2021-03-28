59141031430d2a3c2d7676fdf91ebc39b6a5066a
tanc <mail@tanc.me>
2021-03-28 19:21:35 +0200

    First commit

---
+++ CHANGELOG.md
@@ -0,0 +1,25 @@
+## [0.2.1](https://github.com/ElMassimo/vite-plugin-sfc-reload/compare/v0.2.0...v0.2.1) (2021-03-20)
+
+### Features
+
+- Add `delay` to offset slow template compilation in Rails ([e69c6b6](https://github.com/ElMassimo/vite-plugin-sfc-reload/commit/e69c6b6616a69f7778ef7d34e0a0648fa4dfcbc8))
+
+# [0.2.0](https://github.com/ElMassimo/vite-plugin-sfc-reload/compare/v0.1.3...v0.2.0) (2021-03-20)
+
+- Switch to use Vite's watcher instead of creating a new one with `chokidar`.
+
+## [0.1.3](https://github.com/ElMassimo/vite-plugin-sfc-reload/compare/v0.1.2...v0.1.3) (2021-03-18)
+
+- Move `chalk` and `chokidar` as `dependencies` since only npm v7 installs `peerDependencies`.
+
+## [0.1.2](https://github.com/ElMassimo/vite-plugin-sfc-reload/compare/v0.1.1...v0.1.2) (2021-03-18)
+
+- Add `vite-plugin` keyword to the package to make it easier to find.
+
+## [0.1.1](https://github.com/ElMassimo/vite-plugin-sfc-reload/compare/v0.1.0...v0.1.1) (2021-03-17)
+
+### Bug Fixes
+
+- Remove postinstall hooks on released package ([99db8e2](https://github.com/ElMassimo/vite-plugin-sfc-reload/commit/99db8e2717537268850256e7b3e24358ad9a29b3))
+
+## [0.1.0](https://github.com/ElMassimo/js_from_routes/tree/v0.1.0) (2021-03-16)
---
+++ src/index.ts
@@ -0,0 +1,136 @@
+import { resolve, relative } from 'path'
+import { green, red, dim } from 'chalk'
+import picomatch from 'picomatch'
+import type { Plugin, ViteDevServer } from 'vite'
+import fetch from 'node-fetch'
+
+/**
+ * Configuration for the watched paths.
+ */
+interface Config {
+  /**
+   * Whether full reload should happen regardless of the file path.
+   * @default true
+   */
+  always?: boolean
+
+  /**
+   * How many milliseconds to wait before reloading the page after a file change.
+   * @default 0
+   */
+  delay?: number
+
+  /**
+   * Whether to log when a file change triggers a full reload.
+   * @default true
+   */
+  log?: boolean
+
+  /**
+   * Files will be resolved against this path.
+   * @default process.cwd()
+   */
+  root?: string
+
+  /**
+   * The (local) domain the Drupal site can be found at.
+   * @default 'http://localhost'
+   */
+  domain?: string
+}
+
+interface responseData {
+  template: boolean
+
+  css: boolean
+
+  js: boolean
+}
+
+/**
+ * Allows to automatically reload the page when a watched file changes.
+ */
+export default (paths: string | string[], config: Config = {}): Plugin => ({
+  name: 'vite-plugin-sfc-reload',
+
+  apply: 'serve',
+
+  // NOTE: Enable globbing so that Vite keeps track of the template files.
+  config: () => ({ server: { watch: { disableGlobbing: false } } }),
+
+  configureServer({ watcher, ws, config: { logger } }: ViteDevServer) {
+    const {
+      root = process.cwd(),
+      log = true,
+      always = true,
+      delay = 0,
+      domain = 'http://localhost',
+    } = config
+
+    const files = Array.from(paths).map((path) => resolve(root, path))
+    const shouldReload = picomatch(files)
+    const checkReload = async (path: string) => {
+      if (shouldReload(path)) {
+        // Todo: full reload on Drupal template files.
+
+        // Try the SFC endpoint on the Drupal site. This requires having the
+        // sfc_dev module enabled so print a message if a 404 is returned.
+        const checkStatus = (response: any) => {
+          if (response.ok) {
+            // response.status >= 200 && response.status < 300
+            return response
+          } else {
+            throw 'Failure'
+          }
+        }
+
+        const filePath = relative(root, path)
+        const fetchUrl = `${domain}/sfc/compile`
+
+        const response = await fetch(fetchUrl, {
+          method: 'POST',
+          body: JSON.stringify({ componentPath: filePath }),
+          headers: { 'Content-Type': 'application/json' },
+        })
+
+        try {
+          if (checkStatus(response)) {
+            const responseData = (await response.json()) as responseData
+            if (
+              Object.keys(responseData).length > 0 &&
+              responseData.template === true
+            ) {
+              setTimeout(
+                () =>
+                  ws.send({ type: 'full-reload', path: always ? '*' : path }),
+                delay
+              )
+              if (log)
+                logger.info(
+                  `${green('template update caused page reload')} ${dim(
+                    relative(root, path)
+                  )}`,
+                  { clear: true, timestamp: true }
+                )
+            }
+          }
+        } catch {
+          if (log)
+            logger.info(
+              `${red(`unable to access ${fetchUrl}`)} ${dim(
+                'have you enabled the sfc_dev module?'
+              )}`,
+              { clear: false, timestamp: true }
+            )
+        }
+      }
+    }
+
+    // Ensure Vite keeps track of the files and triggers HMR as needed.
+    watcher.add(files)
+
+    // Do a full page reload if any of the watched files changes.
+    watcher.on('add', checkReload)
+    watcher.on('change', checkReload)
+  },
+})
